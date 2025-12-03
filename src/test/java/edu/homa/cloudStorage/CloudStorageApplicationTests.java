7905e4c8a4b401eaf02e1013769f007659c3057b
Максим <homa19222@gmail.com>
2025-12-03 15:36:10 +0300

    Initial commit with project structure

---
+++ src/test/java/edu/homa/cloudStorage/CloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.homa.cloudStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
