fbc0db1b1e3d88f7fd59740424572d4e876f36f6
homasapienss <homa19222@gmail.com>
2026-02-27 20:59:14 +0300

    added repositories for models

---
+++ src/main/java/edu/homa/cloudStorage/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package edu.homa.cloudStorage.repositories;
+
+import edu.homa.cloudStorage.entities.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+    Optional<RoleEntity> findByName(String name);
+}
