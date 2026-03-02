8aba148231a033220750c396934e0861b00b0d13
homasapienss <homa19222@gmail.com>
2026-03-02 16:09:57 +0300

    добавление перехватчиков для исключений

--- src/main/java/edu/homa/cloudStorage/exceptions/GlobalExceptionHandler.java
+++ src/main/java/edu/homa/cloudStorage/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,26 @@
 package edu.homa.cloudStorage.exceptions;
 
+import edu.homa.cloudStorage.dto.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> handleUsernameAlreadyExists(UserAlreadyExistsException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorMessage(e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> handleApplicationException(ApplicationException applicationException) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMessage(applicationException.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> handleException(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMessage(ex.getMessage()));
+    }
 }
